37c35183ddb3ab988e09f825608518fc334ccc54
itsaFan <steffansim@gmail.com>
2023-12-12 11:13:20 +0800

    add delay

--- src/utils/animate-variants.jsx
+++ src/utils/animate-variants.jsx
@@ -2,11 +2,11 @@ export const navVariants = {
   open: {
     opacity: 1,
     y: 0,
-    transition: { type: "spring", stiffness: 100 },
+    transition: { type: "spring", stiffness: 100, delay: 0.25 },
   },
   closed: {
     opacity: 0,
     y: -20,
-    transition: { type: "spring", stiffness: 100 },
+    transition: { type: "spring", stiffness: 100, delay: 0.25 },
   },
 };
