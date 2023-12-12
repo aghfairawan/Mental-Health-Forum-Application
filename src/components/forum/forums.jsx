b0bbbe36cbaee40b02adf61175ee4936d5b83733
itsaFan <steffansim@gmail.com>
2023-12-12 14:05:53 +0800

    add forum layout

--- src/components/forum/forums.jsx
+++ src/components/forum/forums.jsx
@@ -5,10 +5,7 @@ export default function Forums() {
   return (
     <motion.div layout className="w-full px-4 py-6 bg-white border border-gray-200 rounded-lg shadow  dark:bg-main-dark dark:border-none">
       <div className="flex items-center justify-between mb-4">
-        <h5 className="text-xl font-bold leading-none  text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-pink-500">Todo List </h5>
-        <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-          View all
-        </a>
+        <h5 className="text-xl font-bold leading-none  text-dark-navy">Forums </h5>
       </div>
       <ForumItems />
     </motion.div>
