38b1314e669cea2837d11dd70f8e7e083bea3a81
itsaFan <steffansim@gmail.com>
2023-12-14 18:40:39 +0800

    add socket reconnection attempt

--- src/api/socket-client.js
+++ src/api/socket-client.js
@@ -1,3 +1,8 @@
 import io from "socket.io-client";
 
-export const socket = io("http://localhost:8080");
+export const socket = io("http://localhost:8080", {
+  reconnection: true,
+  reconnectionAttempts: 3,
+  reconnectionDelay: 5 * 1000,
+  reconnectionDelayMax: 60 * 1000,
+});
