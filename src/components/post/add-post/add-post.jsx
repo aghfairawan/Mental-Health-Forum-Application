e8eb5606b0eb8736623a594d9388c45ade370c25
itsaFan <steffansim@gmail.com>
2023-12-19 21:04:02 +0800

    add conditional rendering

--- src/components/post/add-post/add-post.jsx
+++ src/components/post/add-post/add-post.jsx
@@ -14,6 +14,7 @@ export default function AddPost() {
   const titleRef = useRef(null);
   const contentRef = useRef(null);
   const [selectedForum, setSelectedForum] = useState("");
+  const { isLoggedIn } = useAuth();
 
   const onCloseModal = () => {
     setOpenModal(false);
@@ -59,23 +60,33 @@ export default function AddPost() {
   //   console.log(forums);
   return (
     <>
-      <button
-        data-modal-target="post-modal"
-        data-modal-toggle="post-modal"
-        className="flex items-center gap-1 text-white bg-secondary-navy hover:bg-dark-navy focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center"
-        type="button"
-        onClick={() => setOpenModal(true)}
-      >
-        <PenSquare size={20} />
-        New Post
-      </button>
-      <Modal show={openModal} size="md" onClose={onCloseModal} position="top-center" className="pt-10" popup>
-        <Modal.Header />
-        <Modal.Body>
-          <h3 className="text-xl font-medium text-gray-900 dark:text-white mb-6">Create a New Post</h3>
-          <CreatePostForm forums={forums} titleRef={titleRef} contentRef={contentRef} onSubmit={handleAddPost} onForumSelect={handleForumSelect} />
-        </Modal.Body>
-      </Modal>
+      {isLoggedIn ? (
+        <>
+          <button
+            data-modal-target="post-modal"
+            data-modal-toggle="post-modal"
+            className="flex items-center gap-1 text-white bg-secondary-navy hover:bg-dark-navy focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center"
+            type="button"
+            onClick={() => setOpenModal(true)}
+          >
+            <PenSquare size={20} />
+            New Post
+          </button>
+          <Modal show={openModal} size="md" onClose={onCloseModal} position="top-center" className="pt-10" popup>
+            <Modal.Header />
+            <Modal.Body>
+              <h3 className="text-xl font-medium text-gray-900 dark:text-white mb-6">Create a New Post</h3>
+              <CreatePostForm forums={forums} titleRef={titleRef} contentRef={contentRef} onSubmit={handleAddPost} onForumSelect={handleForumSelect} />
+            </Modal.Body>
+          </Modal>
+        </>
+      ) : (
+        <div className="mt-4 h-auto w-full bg-neutral-50 shadow-xl flex flex-row justify-evenly ">
+          <div className="w-full flex px-4 py-3 text-gray-200 rounded-lg  bg-secondary-navy">
+            <span className="font-semibold text-sm italic underline underline-offset-2">You Must logged-in to Create a Post</span>
+          </div>
+        </div>
+      )}
     </>
   );
 }
