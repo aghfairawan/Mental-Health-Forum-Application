6be79187b0125ec5f5161eb164e0c0d09c3e966d
itsaFan <steffansim@gmail.com>
2023-12-19 16:19:21 +0800

    fix route path

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           element: <ForumDetailPage />,
         },
         {
-          path: "/forum/post",
+          path: "/forum/post/:postTitle",
           element: <PostPage />,
         },
         {
--- src/components/post/post-lists.jsx
+++ src/components/post/post-lists.jsx
@@ -2,7 +2,7 @@
 import { FolderOpen } from "lucide-react";
 import PostListCard from "../ui/post-list-card";
 import { motion } from "framer-motion";
-import { convertToInitial } from "../../utils/helper-converter";
+import { convertToInitial, createSlug } from "../../utils/helper-converter";
 import { springVariants } from "../../utils/animate-variants";
 import { formatDateV2 } from "../../utils/date-converter";
 // import { useParams } from "react-router-dom";
@@ -15,9 +15,9 @@ export default function PostLists({ posts, loading, currentPage, onPageChange, t
   // const title = params.title;
   const forumTitle = posts.length > 0 ? posts[0].forum.title : "Forum";
   const navigate = useNavigate();
-  const onClickPost = (postId) => {
-    navigate("/forum/post", { state: { postId } });
-  };
+  // const onClickPost = (postId) => {
+  //   navigate(`/forum/post/${post.title}`, { state: { postId } });
+  // };
 
   return (
     <>
@@ -29,48 +29,54 @@ export default function PostLists({ posts, loading, currentPage, onPageChange, t
             </div>
           ) : (
             <>
-              {posts.map((post) => (
-                <motion.div key={post._id} variants={springVariants} initial="initial" animate="animate" exit="exit" layout className="grid md:grid-cols-5 mb-2 pt-2 gap-2 ">
-                  <div className="flex gap-2 md:col-span-3">
-                    <div className="flex gap-2 justify-start items-center">
-                      <div className="relative text-lg font-medium inline-flex items-center justify-center w-12 h-12 overflow-hidden bg-gray-300 rounded-full" title={post.author.username}>
-                        {convertToInitial(post.author.username)}
+              {posts.map((post) => {
+                const postSlug = createSlug(post.title);
+                const onClickPost = () => {
+                  navigate(`/forum/post/${postSlug}`, { state: { postId: post._id } });
+                };
+                return (
+                  <motion.div key={post._id} variants={springVariants} initial="initial" animate="animate" exit="exit" layout className="grid md:grid-cols-5 mb-2 pt-2 gap-2 ">
+                    <div className="flex gap-2 md:col-span-3">
+                      <div className="flex gap-2 justify-start items-center">
+                        <div className="relative text-lg font-medium inline-flex items-center justify-center w-12 h-12 overflow-hidden bg-gray-300 rounded-full" title={post.author.username}>
+                          {convertToInitial(post.author.username)}
+                        </div>
+                      </div>
+                      <div className="">
+                        <div>
+                          <p className="text-base font-medium line-clamp-1 hover:underline underline-offset-2" onClick={() => onClickPost(post._id)}>
+                            {post.title}
+                          </p>
+                        </div>
+                        <p className="line-clamp-1 text-xs font-normal">{post.content}</p>
+                        <span className="text-xs font-medium">{formatDateV2(post.createdAt)}</span>
                       </div>
                     </div>
-                    <div className="">
-                      <div>
-                        <p className="text-base font-medium line-clamp-1 hover:underline underline-offset-2" onClick={() => onClickPost(post._id)}>
-                          {post.title}
-                        </p>
-                      </div>
-                      <p className="line-clamp-1 text-xs font-normal">{post.content}</p>
-                      <span className="text-xs font-medium">{formatDateV2(post.createdAt)}</span>
+                    <div className="flex  md:justify-center items-center">
+                      <span className="text-xs font-medium">Replies: {post.totalComments} </span>
                     </div>
-                  </div>
-                  <div className="flex  md:justify-center items-center">
-                    <span className="text-xs font-medium">Replies: {post.totalComments} </span>
-                  </div>
-                  <div className="flex justify-start items-center ">
-                    {post.comments.length > 0 ? (
-                      post.comments.map((comment) => (
-                        <div key={comment._id} className="text-sm flex gap-1">
-                          <div className="flex gap-2 justify-start items-center">
-                            <div className="relative text-xs font-medium inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-300 rounded-full" title={post.author.username}>
-                              {convertToInitial(comment.commenter.username)}
+                    <div className="flex justify-start items-center ">
+                      {post.comments.length > 0 ? (
+                        post.comments.map((comment) => (
+                          <div key={comment._id} className="text-sm flex gap-1">
+                            <div className="flex gap-2 justify-start items-center">
+                              <div className="relative text-xs font-medium inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-300 rounded-full" title={post.author.username}>
+                                {convertToInitial(comment.commenter.username)}
+                              </div>
+                            </div>
+                            <div className="flex flex-col gap-0">
+                              <span className="line-clamp-1 text-sm">{comment.text}</span>
+                              <span className="text-xs">{formatDateV2(comment.createdAt)}</span>
                             </div>
                           </div>
-                          <div className="flex flex-col gap-0">
-                            <span className="line-clamp-1 text-sm">{comment.text}</span>
-                            <span className="text-xs">{formatDateV2(comment.createdAt)}</span>
-                          </div>
-                        </div>
-                      ))
-                    ) : (
-                      <span className="text-sm text-rose-700">No comment yet</span>
-                    )}
-                  </div>
-                </motion.div>
-              ))}
+                        ))
+                      ) : (
+                        <span className="text-sm text-rose-700">No comment yet</span>
+                      )}
+                    </div>
+                  </motion.div>
+                );
+              })}
               <div className="flex pt-5 pb-2 overflow-x-auto sm:justify-end">
                 <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} />
               </div>
