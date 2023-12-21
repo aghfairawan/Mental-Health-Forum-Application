82cb2911167a9542f6e20d9160631f6f50d12508
Fahri <68985644+fahriwps@users.noreply.github.com>
2023-12-21 21:42:47 +0700

    add ability to change user default avatar

--- src/components/userProfile/userProfile.jsx
+++ src/components/userProfile/userProfile.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import { PenLine } from 'lucide-react';
-import ThisPage from '../setting-profile/thisPage';
+import { useState } from "react";
+import { PenLine } from "lucide-react";
+import ThisPage from "../setting-profile/thisPage";
 
 const UserProfileForm = () => {
   const initialUserData = {
-    username: 'JohnDoe',
-    address: '123 Main St',
-    country: 'USAA',
-    bio: 'This is my bio. I love coding and exploring new technologies.',
+    username: "JohnDoe",
+    address: "123 Main St",
+    country: "USAA",
+    bio: "This is my bio. I love coding and exploring new technologies.",
   };
 
   const [formData, setFormData] = useState({
@@ -18,9 +18,34 @@ const UserProfileForm = () => {
   });
 
   const [isEditing, setIsEditing] = useState(false);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
 
   const handleProfileChange = () => {
     setIsEditing(!isEditing);
+  };
+  // TODO: move logic API into api module/folder
+  // TODO: change fetch api to axios
+  const handleAvatarChange = (e) => {
+    const file = e.target.files[0];
+    console.log("Selected avatar file:", file);
+
+    if (file) {
+      const formData = new FormData();
+      formData.append("image", file);
+
+      // Make a POST request to the BE with image data
+      fetch("https://final-project-image-wo3wakb2jq-wl.a.run.app/api/profile", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setUploadedImageUrl(data.imageUrl);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
   };
 
   const handleChange = (e) => {
@@ -32,8 +57,13 @@ const UserProfileForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted:', formData);
+    console.log("Form submitted:", formData);
     setIsEditing(false);
+  };
+
+  const handleSubmitAvatar = (e) => {
+    e.preventDefault();
+    console.log("Avatar form submitted");
   };
 
   return (
@@ -43,13 +73,18 @@ const UserProfileForm = () => {
           <div className="mb-8">
             <div className="flex flex-col md:flex-row">
               <div className="w-full md:w-1/4 bg-gray-100">
-                <h2 className="text-2xl font-bold mb-6 md:mb-0 ml-10">Profile</h2>
+                <h2 className="text-2xl font-bold mb-6 md:mb-0 ml-10">
+                  Profile
+                </h2>
               </div>
 
               <div className="w-full md:w-3/4 bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
                 <form onSubmit={handleSubmit} className="p-6">
                   <div className="mb-4 md:ml-10 flex items-center">
-                    <label htmlFor="username" className="block text-sm font-medium text-gray-700 w-16">
+                    <label
+                      htmlFor="username"
+                      className="block text-sm font-medium text-gray-700 w-16"
+                    >
                       Username
                     </label>
                     {isEditing ? (
@@ -66,7 +101,10 @@ const UserProfileForm = () => {
                     )}
                   </div>
                   <div className="mb-4 md:ml-10 flex items-center">
-                    <label htmlFor="bio" className="block text-sm font-medium text-gray-700 w-16">
+                    <label
+                      htmlFor="bio"
+                      className="block text-sm font-medium text-gray-700 w-16"
+                    >
                       Address
                     </label>
                     {isEditing ? (
@@ -82,7 +120,10 @@ const UserProfileForm = () => {
                     )}
                   </div>
                   <div className="mb-4 md:ml-10 flex items-center">
-                    <label htmlFor="country" className="block text-sm font-medium text-gray-700 w-16">
+                    <label
+                      htmlFor="country"
+                      className="block text-sm font-medium text-gray-700 w-16"
+                    >
                       Country
                     </label>
                     {isEditing ? (
@@ -99,7 +140,10 @@ const UserProfileForm = () => {
                     )}
                   </div>
                   <div className="mb-4 md:ml-10 flex items-center">
-                    <label htmlFor="bio" className="block text-sm font-medium text-gray-700 w-16">
+                    <label
+                      htmlFor="bio"
+                      className="block text-sm font-medium text-gray-700 w-16"
+                    >
                       Bio
                     </label>
                     {isEditing ? (
@@ -121,7 +165,7 @@ const UserProfileForm = () => {
                       onClick={handleProfileChange}
                     >
                       <PenLine className="mr-5" />
-                      {isEditing ? 'Save' : 'Change'}
+                      {isEditing ? "Save" : "Change"}
                     </button>
                   </div>
                 </form>
@@ -129,46 +173,70 @@ const UserProfileForm = () => {
             </div>
           </div>
           {/* second form */}
-          <div className='mb-8'>
+          <div className="mb-8">
             <div className="flex flex-col md:flex-row">
               <div className="w-full md:w-1/4 bg-gray-100">
-                <h2 className="text-2xl font-bold mb-6 md:mb-0 ml-10">Avatar</h2>
+                <h2 className="text-2xl font-bold mb-6 md:mb-0 ml-10">
+                  Avatar
+                </h2>
               </div>
 
               <div className="w-full md:w-3/4 bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
-                <form onSubmit={handleSubmit} className="p-6">
+                <form onSubmit={handleSubmitAvatar} className="p-6">
                   <div className="mb-4 md:ml-40 flex-col items-center ">
-                    <div className="rounded-full overflow-hidden w-40 h-40 mb-4">
+                    <label
+                      htmlFor="avatar"
+                      className="block text-sm font-medium text-gray-700 w-16"
+                    >
+                      Change your avatar:
+                    </label>
+                    {uploadedImageUrl ? (
                       <img
-                        src="https://cdn-icons-png.flaticon.com/512/21/21104.png"
+                        src={uploadedImageUrl}
                         alt="Avatar"
-                        className="w-full h-full object-cover"
+                        className="rounded-full overflow-hidden w-40 h-40 mb-4"
                       />
-                    </div>
+                    ) : (
+                      <div className="rounded-full overflow-hidden w-40 h-40 mb-4">
+                        <img
+                          src="https://cdn-icons-png.flaticon.com/512/21/21104.png"
+                          alt="Avatar"
+                          className="w-full h-full object-cover"
+                        />
+                      </div>
+                    )}
+                    <input
+                      type="file"
+                      id="avatar"
+                      name="avatar"
+                      onChange={handleAvatarChange}
+                      accept="image/*"
+                      className="rounded-full overflow-hidden w-40 h-40 mb-4"
+                    />
                     <button
-                      type="button"
+                      type="submit"
                       className="px-8 py-2 bg-blue-500 text-white rounded-md flex items-center hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-                      onClick={handleSubmit}
                     >
                       <PenLine className="mr-5" />
                       Change
                     </button>
-                    <p className="text-gray-600 mt-2">Please ensure your avatar adheres to the community rules. <br />
-                      This means it must be suitable for all ages. i.e. no nudity, profanity or suggestive content.</p>
+                    <p className="text-gray-600 mt-2">
+                      Please ensure your avatar adheres to the community rules.{" "}
+                      <br />
+                      This means it must be suitable for all ages. i.e. no
+                      nudity, profanity or suggestive content.
+                    </p>
                   </div>
                 </form>
               </div>
             </div>
           </div>
         </div>
-        <div className="h-full md:w-1/6 ml-20 mt-5" >
-
+        <div className="h-full md:w-1/6 ml-20 mt-5">
           <ThisPage />
-        </div >
+        </div>
       </div>
     </div>
-
-
   );
 };
 
