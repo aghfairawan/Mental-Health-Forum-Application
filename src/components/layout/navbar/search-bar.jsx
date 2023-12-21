5301e1ba4b30174ca04df9371fd6936d22df7e8d
itsaFan <steffansim@gmail.com>
2023-12-21 16:04:24 +0800

    add loading state

--- src/components/layout/navbar/search-bar.jsx
+++ src/components/layout/navbar/search-bar.jsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
 import { searchApi } from "../../../api/forum-api";
 import { Search } from "lucide-react";
+import { Spinner } from "flowbite-react";
 
 export default function SearchNavbar() {
   const [query, setQuery] = useState("");
-  const [searchResults, setSearchResults] = useState([]); 
+  const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(false);
   // console.log(searchResults)
 
   const handleSearch = async (e) => {
     e.preventDefault();
+    setLoading(true);
     if (!query) return;
     try {
       const data = await searchApi(query, 1);
       setSearchResults(data);
     } catch (error) {
       console.error("Error during search:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -36,8 +41,14 @@ export default function SearchNavbar() {
           className="block w-80 p-1 ps-10 text-sm border border-blue-600 rounded-2xl bg-white focus:ring-1 focus:ring-blue-600 placeholder-gray-400 text-custom-gray"
         />
         <button type="submit" className="absolute top-0 end-0 px-3  text-sm font-medium h-full bg-dark-navy md:bg-light-navy rounded-e-2xl border border-none hover:opacity-80 focus:ring-2 focus:outline-none focus:ring-blue-300 ">
-          <Search size={16} />
-          <span className="sr-only">Search</span>
+          {loading ? (
+            <Spinner className="flex" size="sm" />
+          ) : (
+            <>
+              <Search size={16} />
+              <span className="sr-only">Search</span>
+            </>
+          )}
         </button>
       </div>
     </form>
